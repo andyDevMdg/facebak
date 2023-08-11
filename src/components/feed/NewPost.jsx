594fb0dabc6f76298df9ea8916cf49257d7a3c2c
andyDevMdg <andyavotiana@gmail.com>
2023-08-12 02:17:26 +0300

    Add FontAwesome package and NewPostField with its style

--- src/components/feed/NewPost.jsx
+++ src/components/feed/NewPost.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./newPost.css";
+import { Avatar, Stack } from "@mui/material";
 
 function NewPost() {
+  const [userName, setUserName] = useState("Andy");
+  const [showField, setShowField] = useState(false);
+
   return (
     <div className="newPostContainer">
-      <div>NewPost</div>
+      {showField && (
+        <div className="newStatutFieldBg">
+          <div className="newStatutField">
+            <form>
+              <input type="text" placeholder="What's up ?" />
+            </form>
+          </div>
+        </div>
+      )}
+      <Stack
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="center"
+        spacing={2}
+        sx={{ width: "100%" }}
+      >
+        <Avatar>A</Avatar>
+        <button
+          className="btnStatutField"
+          onClick={() => setShowField(!showField)}
+        >{`What's Up ${userName}`}</button>
+      </Stack>
     </div>
   );
 }
