ea956fa0f998f81baa2bc41d26de3443a5ba43bd
Mii <32428593+MiiJack@users.noreply.github.com>
2023-08-12 13:37:42 +0300

    feat : "Added auth Placeholder"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/homePage/Home';
+import Home from './pages/homePage/Home'
+import Login from './pages/authPage/Login'
+import SignUp from "./pages/authPage/SignUp";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/Signup' element={<SignUp/>}/>
+        </Routes>
   );
 }
 
