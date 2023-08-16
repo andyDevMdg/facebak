b24a144d3f83c86cd31c2af3bc7cd819eb46593d
Fiantso Harena <fiantsorav@gmail.com>
2023-08-16 20:35:12 +0300

    fix: Reinitilize the main project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import Home from "./pages/homePage/Home";
-import { Route, Routes } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: "Poppins, sans-serif",
-  },
-});
-
-function App() {
+const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </ThemeProvider>
-  );
+    <div className="app">
+      Hello World
+    </div>);
 }
 
 export default App;
