b5697dd6e0e295e56705f595d48205524878d265
Fiantso Harena <fiantsorav@gmail.com>
2023-08-16 22:03:46 +0300

    fix: Import the redux store the main application component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 const App = () => {
+  /* Obtient le mode du thème depuis le state Redux */
+  const mode = useSelector((state) => state.mode);
+
+  /* Crée un thème basé sur les paramètres du mode actuel */
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
