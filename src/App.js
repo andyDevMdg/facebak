decfc62d559619f6acd3608e5ebfedbab85675a2
Fiantso Harena <fiantsorav@gmail.com>
2023-08-16 21:05:07 +0300

    fix: Fix the route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="app">
-      Hello World
-    </div>);
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
