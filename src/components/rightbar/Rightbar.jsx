d50781fd93f8dcf3762d9de4bb9d728ff88871c2
andyDevMdg <andyavotiana@gmail.com>
2023-08-17 22:19:27 +0300

    feat: change rightbar component  into a Stack of followed users

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,67 +1,68 @@
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import React from "react";
 import "./rightbar.css";
 
 function Rightbar() {
+  const Item = styled(Paper)(({ theme }) => ({
+    borderRadius: '10px',
+    boxShadow: "none",
+    backgroundColor: "transparent",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    cursor: "pointer",
+    textAlign: "center",
+    color: "#F5EBFF",
+    ":hover": { backgroundColor: "rgba(255,255,255,0.1)" },
+    minWidth: 400,
+  }));
 
-  const drawerWidth = 240;
-
+  const message = `User you followed or friends you added`;
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="right"
+    <Box
+      sx={{ flexGrow: 1, overflow: "hidden", px: 3 }}
+      className="rightbarContainer"
+    >
+      <Stack
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="flex-end"
+        spacing={1}
       >
-        <Toolbar />
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+        <Item
+          sx={{
+            my: 1,
+            mx: "auto",
+            p: 2,
+          }}
+        >
+          <Stack spacing={2} direction="row" alignItems="center">
+            <Avatar>W</Avatar>
+            <Typography>{message}</Typography>
+          </Stack>
+        </Item>
+        <Item
+          sx={{
+            my: 1,
+            mx: "auto",
+            p: 2,
+          }}
+        >
+          <Stack spacing={2} direction="row" alignItems="center">
+            <Stack>
+              <Avatar>W</Avatar>
+            </Stack>
+            <Stack sx={{ minWidth: 0 }}>
+              <Typography>{message}</Typography>
+            </Stack>
+          </Stack>
+        </Item>
+      </Stack>
     </Box>
   );
 }
