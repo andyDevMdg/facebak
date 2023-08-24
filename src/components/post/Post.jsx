ca9128e02af5190962c669207e13abfb7e4c222c
Fiantso Harena <fiantsorav@gmail.com>
2023-08-25 00:13:50 +0300

    fix: Finish the implementation of the feed component

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,38 @@
+import { CommentOutlined, HeartBrokenOutlined, MoreVert } from "@mui/icons-material";
+import UserImgTest from "./1.jpg";
+import PostImgTest from "./2.jpg";
 import "./post.css";
 
 const Post = () => {
-  return <div className="post">Post</div>;
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img src={UserImgTest} className="postProfileImg" alt="" />
+            <span className="postUsername">Fiantso Harena</span>
+            <span className="postDate">5 days ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hello Stivebook ! This is my first post here✨️</span>
+          <img src={PostImgTest} className="postImg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <HeartBrokenOutlined className="likeIcon"/>
+            <span className="postLikeCounter">69 peoples like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">69 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
