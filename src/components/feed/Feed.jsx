f4e84d0e21351b1053d3ce2087f7cf686800e94c
Fiantso Harena <fiantsorav@gmail.com>
2023-08-24 22:39:13 +0300

    feat: Add the share component to feed component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,11 @@
+import Share from "../share/Share";
 import "./feed.css";
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feedWrapper">
-        
+        <Share />
       </div>
     </div>
   );
