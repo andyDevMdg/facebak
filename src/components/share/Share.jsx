c46f45c05d9cd94bd9228db7afdff456330c1be1
Fiantso Harena <fiantsorav@gmail.com>
2023-08-24 23:27:58 +0300

    style: Finish to style of the the share component

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,4 +1,10 @@
-import { PermMedia } from "@mui/icons-material";
+import {
+  EmojiEmotions,
+  Label,
+  PermMedia,
+  Room,
+  Send,
+} from "@mui/icons-material";
 import UserImageTest from "./1.jpg";
 import "./share.css";
 
@@ -14,10 +20,23 @@ const Share = () => {
         <div className="shareBottom">
           <div className="shareOptions">
             <div className="shareOption">
-              <PermMedia className="shareIcon" />
+              <PermMedia htmlColor="tomato" className="shareIcon" />
               <span className="shareOptionText">Photo or video</span>
             </div>
+            <div className="shareOption">
+              <Label htmlColor="cyan" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor="lightgreen" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
           </div>
+          <button className="shareButton">Share</button>
         </div>
       </div>
     </div>
