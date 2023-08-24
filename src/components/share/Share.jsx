f5fc4ea9cfe4e2904109a526e73a5938bed1947c
Fiantso Harena <fiantsorav@gmail.com>
2023-08-24 22:53:04 +0300

    chore: Implement the main structure of the share component

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,7 +1,27 @@
+import { PermMedia } from "@mui/icons-material";
+import UserImageTest from "./1.jpg";
 import "./share.css";
 
 const Share = () => {
-  return <div>Share</div>;
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          {/* <img className="shareProfileImg" src={UserImageTest} alt="" /> */}
+          <input className="shareInput" placeholder="What's in your mind ?" />
+        </div>
+        <hr />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMedia className="shareIcon" />
+              <span className="shareOptionText">Photo or video</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Share;
