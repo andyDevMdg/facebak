45220b601d0575b3587059f02a4b83ad2ab37457
Fiantso Harena <fiantsorav@gmail.com>
2023-08-25 13:20:10 +0300

    fix: Make the feed dynamic

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,32 +1,39 @@
-import { HeartBrokenOutlined, MoreVert } from "@mui/icons-material";
+import { MoreVert } from "@mui/icons-material";
+import { Users } from "../../dummyData";
 import "./post.css";
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src="../../assets/person/2.jpeg" className="postProfileImg" alt="" />
-            <span className="postUsername">Fiantso Harena</span>
-            <span className="postDate">5 days ago</span>
+            <img
+              src={Users.filter((u) => u.id === post?.userId)[0].profilePicture}
+              className="postProfileImg"
+              alt=""
+            />
+            <span className="postUsername">
+              {Users.filter((u) => u.id === post.userId)[0].username}
+            </span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hello Stivebook ! This is my first post here✨️</span>
-          <img src="../../assets/post/1.jpeg" className="postImg" alt="" />
+          <span className="postText">{post?.desc}</span>
+          <img src={post.photo} className="postImg" alt="" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="../../assets/like.png" className="likeIcon"/>
-            <img src="../../assets/heart.png" className="likeIcon"/>
-            <span className="postLikeCounter">69 peoples like it</span>
+            <img src="../../assets/like.png" className="likeIcon" />
+            <img src="../../assets/heart.png" className="likeIcon" />
+            <span className="postLikeCounter">{post.like} peoples like it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">69 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
