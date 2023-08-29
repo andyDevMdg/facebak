02a3b2c6d031e2cd77857aec919f8fd826bc01a8
andyDevMdg <andyavotiana@gmail.com>
2023-08-29 22:15:32 +0300

    feat: add edit username and bio static functionnality

--- src/pages/editProfil/EditProfil.jsx
+++ src/pages/editProfil/EditProfil.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import {Box, Typography} from "@mui/material";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faXmark} from "@fortawesome/free-solid-svg-icons";
 import "./editProfil.css";
 
 function EditProfil({closeEdit}) {
+  const [editingUsername, setEditingUsername] = useState(false);
+  const [editingBio, setEditingBio] = useState(false);
+
+  const [username, setUsername] = useState("Fiantso Harena");
+  const [bio, setBio] = useState("We are a Dreamer");
+
+  const handleEditUsername = () => {
+    setEditingUsername(true);
+  };
+
+  const handleEditBio = () => {
+    setEditingBio(true);
+  };
+
+  const handleSaveChanges = () => {
+    setEditingUsername(false);
+    setEditingBio(false);
+  };
+
   return (
     <div className="editBg">
       <div className="editContainer">
@@ -18,16 +37,58 @@ function EditProfil({closeEdit}) {
           <div className="editUserName">
             <div className="containEdit">
               <Typography variant="h6">Username</Typography>
-              <button className="editBtn">Edit</button>
             </div>
-            <Typography variant="body2" className="infoProfil" >Fiantso Harena</Typography>
+
+            {editingUsername ? (
+              <form className="editForm">
+                <input
+                  type="text"
+                  value={username}
+                  className="inputEdit"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <button className="saveBtn" onClick={handleSaveChanges}>
+                  Save
+                </button>
+              </form>
+            ) : (
+              <>
+                <Typography variant="body2" className="infoProfil">
+                  {username}
+                </Typography>
+              <button className="editUsernameBtn" onClick={handleEditUsername}>
+                Edit
+              </button>
+              </>
+            )}
           </div>
           <div className="editBio">
             <div className="containEdit">
               <Typography variant="h6">Bio</Typography>
-              <button className="editBtn">Edit</button>
             </div>
-            <Typography variant="body2" className="infoProfil" >We are a Dreamer</Typography>
+
+            {editingBio ? (
+              <form className="editForm">
+                <input
+                  type="text"
+                  value={bio}
+                  className="inputEdit"
+                  onChange={(e) => setBio(e.target.value)}
+                />
+                <button className="saveBtn" onClick={handleSaveChanges}>
+                  Save
+                </button>
+              </form>
+            ) : (
+              <>
+                <Typography variant="body2" className="infoProfil">
+                  {bio}
+                </Typography>
+              <button className="editBioBtn" onClick={handleEditBio}>
+                Edit
+              </button>
+              </>
+            )}
           </div>
         </Box>
       </div>
