9c8d66298b233a2e3eabee54ee9746c732259780
Mii <32428593+MiiJack@users.noreply.github.com>
2023-08-29 11:44:18 +0300

    refactor : Added appropriate routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import Home from "./pages/homePage/Home";
-import Login from './pages/authPage/Login'
-import SignUp from "./pages/authPage/SignUp";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/homePage/Home';
+import Login from './pages/authPage/Login';
+import SignUp from './pages/authPage/SignUp';
+import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
+import Post from './pages/Post';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
-  typography: {
-    fontFamily: "Poppins, sans-serif",
-  },
+    typography: {
+        fontFamily: "Poppins, sans-serif",
+    },
 });
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/Signup' element={<SignUp/>}/>
-    </Routes>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/sign-up" component={SignUp} />
+                <Route exact path="/profile" component={Profile} />
+                <Route path="/profile/edit" component={EditProfile} />
+                <Route exact path="/posts" component={Post} />
+                <Route path="/posts/:postId" component={Post} />
+                <Route path="*" component={() => "404 NOT FOUND"} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
